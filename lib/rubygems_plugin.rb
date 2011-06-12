da057e8c993ec7e70d43a7b72ad59daa016eb4e6
mpapis <mpapis@gmail.com>
2011-06-12 16:51:05 +0200

    it is not yet sure if rubygems 1.9 will include regenerate command

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,7 +1,8 @@
+require 'rubygems/command_manager'
+require 'rubygems_bundler/rubygems_bundler_installer'
+require 'rubygems_bundler/regenerate_binstubs_command'
 if Gem::VERSION < '1.9' then
-  require 'rubygems/command_manager'
-  require 'rubygems_bundler/rubygems_bundler_installer'
   require 'rubygems_bundler/fix_wrapper'
-  require 'rubygems_bundler/regenerate_binstubs_command'
-  Gem::CommandManager.instance.register_command :regenerate_binstubs
 end
+
+Gem::CommandManager.instance.register_command :regenerate_binstubs
