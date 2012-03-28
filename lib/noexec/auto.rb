787521f949c19f5b250f829b15a5f3aacd8fb72f
Josh Hull <joshbuddy@gmail.com>
2012-03-28 09:45:29 -0700

    add whitelist of bins to load from

--- lib/noexec/auto.rb
+++ lib/noexec/auto.rb
@@ -21,7 +21,7 @@ begin
       ENV['BUNDLE_GEMFILE'] = gemfile
       Bundler.load.specs.each do |spec|
         next if spec.name == 'bundler'
-        return spec if spec.executables.include?(bin)
+        return spec if %w(ruby irb).include?(bin) || spec.executables.include?(bin)
       end
       nil
     ensure
@@ -30,26 +30,24 @@ begin
 
     def setup
       puts "Noexec" if DEBUG
-      catch(:done) do
-        throw :done if File.basename($0) == 'bundle'
-        throw :done if ENV['BUNDLE_GEMFILE']
-        gemfile = File.join(CURRENT, "Gemfile")
-        while true
-          if File.exist?(gemfile)
-            puts "Examining #{gemfile}" if DEBUG
-            if Noexec.candidate?(gemfile, File.basename($0))
-              puts "Using #{gemfile}" if DEBUG
-              ENV['BUNDLE_GEMFILE'] = gemfile
-              Bundler.setup
-              throw :done
-            end
+      return if File.basename($0) == 'bundle'
+      return if ENV['BUNDLE_GEMFILE']
+      gemfile = File.join(CURRENT, "Gemfile")
+      while true
+        if File.exist?(gemfile)
+          puts "Examining #{gemfile}" if DEBUG
+          if Noexec.candidate?(gemfile, File.basename($0))
+            puts "Using #{gemfile}" if DEBUG
+            ENV['BUNDLE_GEMFILE'] = gemfile
+            Bundler.setup
+            return
           end
-          new_gemfile = File.expand_path("../../Gemfile", gemfile)
-          throw :done if new_gemfile == gemfile
-          gemfile = new_gemfile
         end
-        puts "No valid Gemfile found, moving on" if DEBUG
+        new_gemfile = File.expand_path("../../Gemfile", gemfile)
+        break if new_gemfile == gemfile
+        gemfile = new_gemfile
       end
+      puts "No valid Gemfile found, moving on" if DEBUG
     end
   end
 
