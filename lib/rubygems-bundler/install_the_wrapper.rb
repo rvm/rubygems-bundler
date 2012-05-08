b84f58149542bfb177789ef6e5e93dd88bc48994
Michal Papis <mpapis@gmail.com>
2012-05-08 04:13:34 +0200

    fix finding the installer wrapper

--- lib/rubygems-bundler/install_the_wrapper.rb
+++ lib/rubygems-bundler/install_the_wrapper.rb
@@ -3,13 +3,22 @@ require 'fileutils'
 require 'rubygems'
 
 bindir       = Gem.respond_to?(:bindir,true) ? Gem.send(:bindir) : File.join(Gem.dir, 'bin')
-
 wrapper_name = 'ruby_noexec_wrapper'
-wrapper      = Gem.find_files(   wrapper_name )
 destination  = File.expand_path( wrapper_name, bindir )
 
-unless wrapper.empty? || File.exist?(destination)
+rubygems_bundler_spec =
+  if Gem::Specification.respond_to?(:find_by_name)
+    Gem::Specification.find_by_name("rubygems-bundler")
+  else
+    Gem.source_index.find_name("rubygems-bundler").last
+  end
+
+if rubygems_bundler_spec
+  wrapper_path = File.expand_path( "bin/#{wrapper_name}", rubygems_bundler_spec.full_gem_path )
+end
+
+if rubygems_bundler_spec && File.exist?(wrapper_path) && !File.exist?(destination)
   FileUtils.mkdir_p(bindir, :verbose => true)
-  FileUtils.cp(wrapper.first, destination) 
+  FileUtils.cp(wrapper_path, destination)
   File.chmod(0775, destination)
 end
