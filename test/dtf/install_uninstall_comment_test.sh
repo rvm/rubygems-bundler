c46b99004147738db5b1dbf222221af9155fb5ac
Michal Papis <mpapis@gmail.com>
2012-05-08 22:53:54 +0200

    add tests

---
+++ test/dtf/install_uninstall_comment_test.sh
@@ -0,0 +1,16 @@
+: init
+rake build
+rake install # match=/installed/
+
+gem install haml          # match=/1 gem installed/
+
+head -n 1 $(which haml)   # match=/env ruby_noexec_wrapper/
+which ruby_noexec_wrapper # status=0
+
+gem list                     # match=/haml/
+rubygems-bundler-uninstaller # match=/haml/
+
+head -n 1 $(which haml)   # match!=/env ruby_noexec_wrapper/
+which ruby_noexec_wrapper # status=1
+
+gem uninstall -x haml     # match=/Successfully uninstalled/
