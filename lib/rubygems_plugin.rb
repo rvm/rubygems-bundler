765c0726e3d09498b1503a1bbd8e4afd6d7a2034
Michal Papis <mpapis@gmail.com>
2012-05-12 23:03:36 +0200

    add test for NOEXEC env flag, improve error message for older versions, do not load plugin if running different version of gem then the loaded one

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,16 +1,42 @@
-require 'rubygems/version'
-require 'rubygems-bundler/wrapper'
+rubygems_bundler_spec =
+  if Gem::Specification.respond_to?(:find_by_name)
+    Gem::Specification.find_by_name("rubygems-bundler")
+  else
+    Gem.source_index.find_name("rubygems-bundler").last
+  end
 
-# Set the custom_shebang if user did not set one
-Gem.pre_install do |inst|
-  Gem.configuration[:custom_shebang] ||= '$env ruby_noexec_wrapper'
-  RubygemsBundler::Wrapper.install
+called_version = __FILE__.sub(/^.*\/rubygems-bundler-([^\/]+)\/.*$/,'\1')
+
+# continue only if loaded and called versions all the same
+if rubygems_bundler_spec and rubygems_bundler_spec.version.to_s == called_version
+  require 'rubygems/version'
+  require 'rubygems-bundler/wrapper'
+
+  # Set the custom_shebang if user did not set one
+  Gem.pre_install do |inst|
+    Gem.configuration[:custom_shebang] ||= '$env ruby_noexec_wrapper'
+    RubygemsBundler::Wrapper.install
+  end
+
+  if Gem::Version.new(Gem::VERSION) < Gem::Version.new('2.0') then
+    # Add custom_shebang support to rubygems
+    require 'rubygems-bundler/rubygems_bundler_installer'
+  end
+
+  require 'rubygems-bundler/regenerate_binstubs_command'
+  Gem::CommandManager.instance.register_command :regenerate_binstubs
+elsif ENV.key?('NOEXEC_DEBUG')
+  msg = "Older rubygems-bundler found, loaded: #{rubygems_bundler_spec.version}, called: #{called_version}, you can uninstall it with:\n\n    "
+
+  error_source = __FILE__
+  gem_source = File.expand_path("../../../..", error_source)
+  gem_version = error_source.sub(/^.*\/rubygems-bundler-([^\/]+)\/.*$/,'\1')
+
+  if ENV['GEM_HOME'] or ENV['GEM_PATH']
+    msg << "GEM_HOME=\"#{gem_source}\" " unless ENV['GEM_HOME'] == gem_source
+  end
+
+  msg << "gem uninstall -ax rubygems-bundler -v #{gem_version}\n\n"
+
+  puts msg
 end
-
-if Gem::Version.new(Gem::VERSION) < Gem::Version.new('2.0') then
-  # Add custom_shebang support to rubygems
-  require 'rubygems-bundler/rubygems_bundler_installer'
-end
-
-require 'rubygems-bundler/regenerate_binstubs_command'
-Gem::CommandManager.instance.register_command :regenerate_binstubs
---
+++ test/dtf/rails_and_gemfile_comment_test.sh
@@ -0,0 +1,21 @@
+: init
+typeset test_dir="${TMPDIR:-/tmp}/fdkngfk"
+export GEM_HOME="${test_dir}/fake_gem_home"
+export PATH="${test_dir}/fake_gem_home/bin:$PATH"
+
+mkdir -p "${GEM_HOME}"
+printf "source :rubygems\n\ngem 'rails'\n" > "${test_dir}/Gemfile"
+
+rake build
+rake install        # match=/installed/
+gem install bundler # status=0
+
+: test
+builtin cd "${test_dir}"
+bundle install             # match=/Installing rails/
+NOEXEC=0 rails new app1
+[[ -f app1/Gemfile ]]      # status=0
+[[ -f app1/Gemfile.lock ]] # status=0
+
+: cleanup
+rm -rf "${test_dir}"
