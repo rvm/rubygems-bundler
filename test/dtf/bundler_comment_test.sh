2b42f55908eee0e8cc3b86c0964a2b9990b96e87
Michal Papis <mpapis@gmail.com>
2012-09-15 23:54:01 +0200

    add a test to ensure that bundler is not loaded for heroku if disabled in config

--- test/dtf/bundler_comment_test.sh
+++ test/dtf/bundler_comment_test.sh
@@ -1,20 +1,29 @@
 : init
-export BUNDLE_GEMFILE=${TMPDIR:-/tmp}/Gemfile
-printf "source :rubygems\n\ngem 'haml'\n" > ${BUNDLE_GEMFILE}
+export BUNDLE_GEMFILE=${TMPDIR:-/tmp}/rubygems-bunelr_bundler-test/Gemfile
+mkdir -p ${BUNDLE_GEMFILE%/*} # status=0
+printf "source :rubygems\n\ngem 'haml'\ngem 'heroku'\n" > ${BUNDLE_GEMFILE}
 
-yes | sm gem install # match=/installed/
-gem install bundler # status=0
+yes | sm gem install         # match=/installed/
+gem regenerate_binstubs      # status=0
+gem install bundler          # status=0
 
 bundle install
 
-head -n 1 $(which haml)   # match=/env ruby_noexec_wrapper/
-which ruby_noexec_wrapper # status=0
+: exclusion
+head -n 1 $(which heroku)    # match=/env ruby_noexec_wrapper/
+NOEXEC_DEBUG=1 heroku update # match=/Using .*/rubygems-bunelr_bundler-test/Gemfile/; match!=/Binary excluded by config/
+printf "exclude:\n - heroku\n" > ${BUNDLE_GEMFILE%/*}/.noexec.yaml
+NOEXEC_DEBUG=1 heroku update # match!=/Using .*/rubygems-bunelr_bundler-test/Gemfile/; match=/Binary excluded by config/
+
+: generated/removed
+head -n 1 $(which haml)      # match=/env ruby_noexec_wrapper/
+which ruby_noexec_wrapper    # status=0
 
 gem list                     # match=/haml/
 rubygems-bundler-uninstaller # match=/haml/
 
-head -n 1 $(which haml)   # match!=/env ruby_noexec_wrapper/
-which ruby_noexec_wrapper # status=1
+head -n 1 $(which haml)      # match!=/env ruby_noexec_wrapper/
+which ruby_noexec_wrapper    # status=1
 
-gem uninstall -x haml     # match=/Successfully uninstalled/
-rm -f ${BUNDLE_GEMFILE}
+gem uninstall -x haml        # match=/Successfully uninstalled/
+rm -rf ${BUNDLE_GEMFILE%/*}  # status=0
