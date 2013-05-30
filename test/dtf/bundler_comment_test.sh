d587ad0234f52a37512ddd884a5eecbc27987170
Michal Papis <mpapis@gmail.com>
2013-05-30 23:05:49 +0200

    fix tests after heroku gem was removed

--- test/dtf/bundler_comment_test.sh
+++ test/dtf/bundler_comment_test.sh
@@ -1,7 +1,7 @@
 : init
 export BUNDLE_GEMFILE=${TMPDIR:-/tmp}/rubygems-bunelr_bundler-test/Gemfile
 mkdir -p ${BUNDLE_GEMFILE%/*} # status=0
-printf "source :rubygems\n\ngem 'haml'\ngem 'heroku'\ngem 'jruby-openssl', :require => 'openssl', :platforms => :jruby\n" > ${BUNDLE_GEMFILE}
+printf "source 'https://rubygems.org'\n\ngem 'haml'\ngem 'jruby-openssl', :require => 'openssl', :platforms => :jruby\n" > ${BUNDLE_GEMFILE}
 
 yes | sm gem install         # match=/installed/
 gem regenerate_binstubs      # status=0
@@ -10,10 +10,10 @@ gem install bundler          # status=0
 bundle install
 
 : exclusion
-head -n 1 $(which heroku)    # match=/env ruby_noexec_wrapper/
-yes | NOEXEC_DEBUG=1 heroku update # match=/Using .*/rubygems-bunelr_bundler-test/Gemfile/; match!=/Binary excluded by config/
-printf "exclude:\n - heroku\n" > ${BUNDLE_GEMFILE%/*}/.noexec.yaml
-yes | NOEXEC_DEBUG=1 heroku update # match!=/Using .*/rubygems-bunelr_bundler-test/Gemfile/; match=/Binary excluded by config/
+head -n 1 $(which haml)      # match=/env ruby_noexec_wrapper/
+NOEXEC_DEBUG=1 haml --version # match=/Using .*/rubygems-bunelr_bundler-test/Gemfile/; match!=/Binary excluded by config/
+printf "exclude:\n - haml\n" > ${BUNDLE_GEMFILE%/*}/.noexec.yaml
+NOEXEC_DEBUG=1 haml --version # match!=/Using .*/rubygems-bunelr_bundler-test/Gemfile/; match=/Binary excluded by config/
 
 : generated/removed
 head -n 1 $(which haml)      # match=/env ruby_noexec_wrapper/
@@ -25,5 +25,5 @@ rubygems-bundler-uninstaller # match=/haml/
 head -n 1 $(which haml)      # match!=/env ruby_noexec_wrapper/
 which ruby_noexec_wrapper    # status=1
 
-gem uninstall -x haml        # match=/Successfully uninstalled/
+gem uninstall -ax haml       # match=/Successfully uninstalled/
 rm -rf ${BUNDLE_GEMFILE%/*}  # status=0
