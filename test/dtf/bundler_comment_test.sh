a84540458ff45437d7434d49499193c6ae4cbea8
Michal Papis <mpapis@gmail.com>
2013-07-11 01:50:41 +0200

    switch ruby_noexec_wrapper to ruby_executable_hooks in tests

--- test/dtf/bundler_comment_test.sh
+++ test/dtf/bundler_comment_test.sh
@@ -10,20 +10,20 @@ gem install bundler          # status=0
 bundle install
 
 : exclusion
-head -n 1 $(which haml)      # match=/env ruby_noexec_wrapper/
+head -n 1 $(which haml)      # match=/env ruby_executable_hooks/
 NOEXEC_DEBUG=1 haml --version # match=/Using .*/rubygems-bunelr_bundler-test/Gemfile/; match!=/Binary excluded by config/
 printf "exclude:\n - haml\n" > ${BUNDLE_GEMFILE%/*}/.noexec.yaml
 NOEXEC_DEBUG=1 haml --version # match!=/Using .*/rubygems-bunelr_bundler-test/Gemfile/; match=/Binary excluded by config/
 
 : generated/removed
-head -n 1 $(which haml)      # match=/env ruby_noexec_wrapper/
-which ruby_noexec_wrapper    # status=0
+head -n 1 $(which haml)      # match=/env ruby_executable_hooks/
+which ruby_executable_hooks    # status=0
 
 gem list                     # match=/haml/
 rubygems-bundler-uninstaller # match=/haml/
 
-head -n 1 $(which haml)      # match!=/env ruby_noexec_wrapper/
-which ruby_noexec_wrapper    # status=1
+head -n 1 $(which haml)      # match!=/env ruby_executable_hooks/
+which ruby_executable_hooks    # status=1
 
 gem uninstall -ax haml       # match=/Successfully uninstalled/
 rm -rf ${BUNDLE_GEMFILE%/*}  # status=0
--- test/dtf/rubygems_comment_test.sh
+++ test/dtf/rubygems_comment_test.sh
@@ -3,13 +3,13 @@ sm gem install # match=/installed/
 
 yes | gem install haml          # match=/1 gem installed/
 
-head -n 1 $(which haml)   # match=/env ruby_noexec_wrapper/
-which ruby_noexec_wrapper # status=0
+head -n 1 $(which haml)   # match=/env ruby_executable_hooks/
+which ruby_executable_hooks # status=0
 
 gem list                     # match=/haml/
 rubygems-bundler-uninstaller # match=/haml/
 
-head -n 1 $(which haml)   # match!=/env ruby_noexec_wrapper/
-which ruby_noexec_wrapper # status=1
+head -n 1 $(which haml)   # match!=/env ruby_executable_hooks/
+which ruby_executable_hooks # status=1
 
 gem uninstall -x haml     # match=/Successfully uninstalled/
