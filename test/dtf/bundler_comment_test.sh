a21646f9e5a84ef2e859b4cbd044e9271c948aed
Michal Papis <mpapis@gmail.com>
2013-09-22 01:38:55 +0200

    try bundler --pre

--- test/dtf/bundler_comment_test.sh
+++ test/dtf/bundler_comment_test.sh
@@ -6,9 +6,9 @@ printf "source 'https://rubygems.org'\n\ngem 'haml'\n" | tee ${BUNDLE_GEMFILE}
 
 yes | sm gem install         # match=/installed/
 gem regenerate_binstubs      # status=0
-gem install bundler          # status=0
+gem install bundler --pre    # status=0
 
-env DEBUG_RESOLVER=1 DEBUG=1 bundle install # status=0
+env NOEXEC_DEBUG=1 NOEXEC_DISABLE=1 DEBUG_RESOLVER=1 DEBUG=1 bundle install # status=0
 
 : exclusion
 head -n 1 "$(which haml)"     # match=/env ruby_executable_hooks/
