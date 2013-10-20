49f139dee50bcd992f72ab5fe0de9f52ecdc912f
Michal Papis <mpapis@gmail.com>
2013-10-20 04:08:07 +0200

    cleaning code

--- lib/rubygems-bundler/noexec.rb
+++ lib/rubygems-bundler/noexec.rb
@@ -4,7 +4,7 @@ class Noexec
   DEBUG   = ENV['NOEXEC_DEBUG']
   CURRENT = Dir.pwd
 
-  attr_reader :bin, :gemfile
+  attr_reader :bin, :gemfile, :rubygems_specs
 
   def initialize(bin)
     log "Bin used: #{bin}"
@@ -42,28 +42,26 @@ class Noexec
     end
     ENV['BUNDLE_GEMFILE'] = gemfile
     runtime = Bundler.load
-      if rubygems_spec
-        missing_spec = runtime.
-          instance_variable_get(:@definition).
-          missing_specs.
-          detect{|spec| spec.name == rubygems_spec.name}
-        if missing_spec
-          puts "\e[31mCould not find proper version of #{missing_spec.to_s} in any of the sources\e[0m"
-          puts "\e[33mRun `bundle install` to install missing gems.\e[0m"
-          exit Bundler::GemNotFound.new.status_code
-        end
+    log2 "runtime specs: #{runtime.specs.map{|g| "#{g.name}-#{g.version}"}*" "}"
+    if rubygems_spec # that single gem
+      missing_spec = runtime.
+        instance_variable_get(:@definition).
+        missing_specs.
+        detect{|spec| spec.name == rubygems_spec.name}
+      if missing_spec
+        puts "\e[31mCould not find proper version of #{missing_spec.to_s} in any of the sources\e[0m"
+        puts "\e[33mRun `bundle install` to install missing gems.\e[0m"
+        exit Bundler::GemNotFound.new.status_code
       end
-      log2 "runtime specs: #{runtime.specs.map{|g| "#{g.name}-#{g.version}"}*" "}"
-      return true if runtime.specs.detect{ |spec| spec.executables.include?(bin) }
+    end
+    if runtime.specs.detect{ |spec| spec.executables.include?(bin) }
+      return true
+    end
     Bundler.unload!(rubygems_specs)
     false
   rescue Bundler::BundlerError, Bundler::GemfileError => e
     warn "Ignoring candidate #{gemfile}:\n#{e}" if Noexec::DEBUG
     false
-  end
-
-  def rubygems_specs
-    @rubygems_specs ||= Bundler.rubygems.plain_specs
   end
 
   def rubygems_spec
@@ -74,7 +72,7 @@ class Noexec
     puts "Noexec - starting check" if Noexec::DEBUG
     require "bundler-unload"
 
-    rubygems_specs # calculate it before entering bundler
+    @rubygems_specs = Bundler.rubygems.plain_specs # save it for unloading and checking binary
     log2 "rubygems_specs: #{rubygems_specs.map{|g| "#{g.name}-#{g.version}"}*" "}"
 
     @gemfile = ENV['BUNDLE_GEMFILE'] || File.join(Noexec::CURRENT, "Gemfile")
